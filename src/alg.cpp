7c13db359a3c1e0cd6ddf397a885a9de5c85c563
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-03-02 01:02:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,7 +45,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   long double answ = 0;
   double k = 2;
-  for (uint64_t i = 1; i <= count; i++) {
+  for (uint64_t i = 1; i < count; i++) {
     answ += pown(-1, k)*pown(x, i)/fact(i);
     i++;
     k++;
