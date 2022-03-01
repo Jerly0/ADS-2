a9d3a4bd336a529ff51149f80bc7ae03ac6d8c1f
Jerly0 <87529148+Jerly0@users.noreply.github.com>
2022-03-01 21:52:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,21 +12,21 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-
+  return 0;
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return 0;
 }
 
 double expn(double x, uint16_t count) {
-
+  return 0;
 }
 
 double sinn(double x, uint16_t count) {
-
+  return 0;
 }
 
 double cosn(double x, uint16_t count) {
-
+  return 0;
 }
